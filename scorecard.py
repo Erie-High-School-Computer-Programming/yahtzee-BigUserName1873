763a1a07b0adcaaff2bfbbf63dee034577e09db5
BigUserName <cantfindaname37@gmail.com>
2023-09-21 15:35:04 -0400

    scoring functions done

--- scorecard.py
+++ scorecard.py
@@ -17,7 +17,7 @@ class Scorecard:
 
         self.bonus_yahtzee = False
     
-    def convert_to_set(list:list):
+    def remove_dups_and_sort(list:list):
         set(list)
         return sorted(set)
 
@@ -92,7 +92,9 @@ class Scorecard:
         return round_score
 
     def yahtzee(self, dice:list):
-        '''Scores the yahtzee. It removes the duplicates from the list using set and tests if the lenght is longer than one.'''
+        '''Scores the yahtzee. 
+        It removes the duplicates from the list using set 
+        and tests if the lenght is longer than one.'''
         round_score = 0
 
         dice_set = set(dice)
@@ -118,14 +120,16 @@ class Scorecard:
         return round_score
 
 
-
     def full_house(self, dice:list):
         '''Scores the full house. It removes the duplicates from the list using set and tests if the lenght is longer than two.'''
         round_score = 0
 
-        dice_set = set(dice)
+        sorted_dice = sorted(dice)
 
-        if len(dice_set) == 2:
+        if (
+            sorted_dice[0] == sorted_dice[1] and sorted_dice[2] == sorted_dice[4] or
+            sorted_dice[0] == sorted_dice[2] and sorted_dice[3] == sorted_dice[4]
+        ):
             self.full_house_used = True
             round_score += 25
 
@@ -137,13 +141,35 @@ class Scorecard:
         dice_check = 0
 
 
-        dice_sorted = self.convert_to_set(dice)
+        dice_sorted = self.remove_dups_and_sort(dice)
 
         for i in range(4):
             if dice_sorted[i] + 1 == dice_sorted[i+1]:
                 dice_check += 1
-        if dice_check == 3:
+        if dice_check >= 3:
             round_score += 30
             self.sm_straight_used = True
 
         return round_score
+
+    def four_of_a_kind(self, dice:list):
+
+        round_score = 0 
+
+        dice_sorted = self.remove_dups_and_sort(dice)
+        
+        if dice_sorted >=2 :
+            dice_score = sum(dice)
+            round_score += dice_score
+
+
+    def theer_of_a_kind(self, dice:list):
+
+        round_score = 0 
+
+        dice_sorted = self.remove_dups_and_sort(dice)
+        
+        if dice_sorted >=3 :
+            dice_score = sum(dice)
+            round_score += dice_score
+
